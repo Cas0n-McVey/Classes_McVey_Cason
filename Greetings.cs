69047e5296e93f1a35ff67fb7703255ef13ecbff
Cas0n-McVey <absolgamer25@gmail.com>
2025-04-02 13:52:39 -0500

    Classes

---
+++ Greetings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_McVey_Cason
+{
+    internal class Greetings
+    {
+        /// <summary>
+        /// Welcoming you
+        /// </summary>
+        public void Welcome()
+        {
+            Console.WriteLine("Hello Traveler, welcome!");
+        }
+        /// <summary>
+        /// Tells you the name you put down and paints it
+        /// </summary>
+        /// <param name="name"></param>
+        public void Hello(string name)
+        {
+            Console.WriteLine($"{name}, thank you for joining us today!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Classes_McVey_Cason
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // It's going to create an object from that class from the greetings class
+            Greetings greetings = new Greetings();
+            // To call Welcome Method in Greetings.cs
+            greetings.Welcome();
+
+            // Asking for your name and paints it out
+            Console.WriteLine("What is your name squire?");
+            string userName = Console.ReadLine();
+
+            // To call Hello Method in Greetings.cs
+            greetings.Hello(userName);
+        }
+    }
+}
+    
+
